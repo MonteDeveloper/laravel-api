38f36e9cc77faa6408b9d3c2b767d95505d848ec
MonteDeveloper <lorenzomontello83@gmail.com>
2023-07-17 15:38:27 +0200

    milestone1: json project:all

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        $response = [
+            "success" => true,
+            "results" => $projects
+        ];
+
+        return response()->json($response);
+    }
+}
